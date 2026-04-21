d4257d10873e495ed8a8a973c4004b515f07d515
DmitryMipt <32105111+DmitryMipt@users.noreply.github.com>
2026-04-22 00:36:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ YOOKASSA_SHOP_ID = "1144426"
 YOOKASSA_SECRET_KEY = "live_eLlF64yoCU9fFPyMrqYSltE71pvOR3B2w4tL0cuCbZ8"
 
 GMAIL_USER = "richkolunov@gmail.com"
-GMAIL_PASSWORD = "nutg wbte icgc rmaj"
+GMAIL_PASSWORD = "obwa sxmu cruz lnkc"
